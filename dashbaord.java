e8d3f9886cc82fee01ea540b495847ff48a9e38b
Laurent Nkwabi <nkwabilaurent518@gmail.com>
2026-05-16 13:32:15 +0300

    adding dashboard page

---
+++ dashbaord.java
@@ -0,0 +1,6 @@
+public class Dashboard {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the dashboard!");
+        System.out.println("Here you can access your account information and settings.");
+    }
+}
