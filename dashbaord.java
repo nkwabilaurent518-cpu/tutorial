c3a6dcecaf31e82e1916408764903f48fa2c4cdb
Laurent Nkwabi <nkwabilaurent518@gmail.com>
2026-05-16 13:39:57 +0300

    switching branch

--- dashbaord.java
+++ dashbaord.java
@@ -1,4 +1,4 @@
-public class Dashboard {
+public class dashboard {
     public static void main(String[] args) {
         System.out.println("Welcome to the dashboard!");
         System.out.println("Here you can access your account information and settings.");
