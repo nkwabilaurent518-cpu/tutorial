24f8d6ccb1f4e3c9fb3ec212a59e5ae54a6143fb
Laurent Nkwabi <nkwabilaurent518@gmail.com>
2026-05-16 13:27:40 +0300

    loginpage

---
+++ login.java
@@ -0,0 +1,6 @@
+public class login {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the login page!");
+        System.out.println("Please enter your username and password.");
+    }
+}
---
+++ tutorial.java
@@ -0,0 +1,7 @@
+public class Tutorial {
+    public static void main(String[] args) {
+        System.out.println("Hello, nkwabi!");
+        System.out.println("Hello, nkwabi!");
+
+    }
+}
